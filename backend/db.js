cc9a53de5247f37f4397f58127d8c0149d236f51
Madhan Raj <madhanraj242003@gmail.com>
2025-09-15 13:18:51 +0530

    updated db.js & modifies .env

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,19 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config(); 
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, 
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "notes_app",
+        password: "Maddy24",
+        port: 5432,
+      }
+);
 
 module.exports = pool;
